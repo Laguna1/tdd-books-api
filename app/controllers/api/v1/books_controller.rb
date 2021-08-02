2906afdbacb6fb90d9bff3da9438aa89c8fec732
Laguna1 <lagunazoomarket@gmail.com>
2021-08-03 00:38:37 +0300

    Update books_controller to use authenticate request

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V1
     class BooksController < ApplicationController
+      before_action :authenticate_request!
       before_action :set_book, only: %i[update show destroy]
 
       # GET /books
@@ -13,7 +14,7 @@ module Api
 
       # POST /book
       def create
-        @book = Book.create(book_params)
+        @book = current_user!.books.create(book_params)
         if @book.save
           render json: BookRepresenter.new(@book).as_json, status: :created
         else
